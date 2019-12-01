d7554c299e9c69b6e7ae177476f2322b636c403d
pray-lee <praycis@gmail.com>
2019-12-01 20:37:29 +0800

    update

--- learning-functional/lib/es6-functional.js
+++ learning-functional/lib/es6-functional.js
@@ -1,7 +1,7 @@
 // 遍历数组
 const forEach = (array, fn) => {
     let i
-    for(i = 0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         fn(array[i])
     }
 }
@@ -11,27 +11,32 @@ export default forEach
 // 遍历对象
 const forEachObject = (obj, fn) => {
     let property
-    for ( property in obj) {
+    for (property in obj) {
         if (obj.hasOwnProperty(property)) {
             fn(property, obj[property])
         }
     }
 }
 
-export {forEachObject}
+export {
+    forEachObject
+}
 
 // 从一个数组中取出偶数
 const unless = (precidate, fn) => {
     if (!precidate)
         fn()
 }
-export {unless}
+export {
+    unless
+}
 
 // 从0-100取出偶数
 const times = (times, fn) => {
-    let i 
-    for (i = 0; i < times; i++) {
+    let i
+    for (i = 0; i < times; i++)
         fn(i)
-    }
 }
-export { times }
+export {
+    times
+}
